53af0bd142d15e9f9a8c07d7725c72e4b28181d6
vidushisaxen <vidushisaxenabdn786@gmail.com>
2023-12-27 19:04:39 +0530

    main

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -12,6 +12,14 @@ import { useState } from 'react';
 
 const Header = () => {
   const[search,setSearch] =  useState("");
+
+  const searchonChange = (event)=>{
+   setSearch(event.target.value);
+  }
+
+  const onSearch =(keyword)=>{
+  console.log('search ' , keyword);
+  }
   return (
 
     <>
@@ -20,8 +28,9 @@ const Header = () => {
     <img src='https://media.istockphoto.com/id/1135090585/vector/letter-p-logo-icon-design.jpg?s=612x612&w=0&k=20&c=lzfzb__61ufKGy-mvrHDriaazQW9iMGj5HK52PQTH9Y=' className='logo'/>
     <Link to = '/Home' className='link'><h2>Home</h2></Link>
     <Link to = '/Explore' className='link'><h2>Explore</h2></Link>
-    <Link to = '/Create' className='link'><h2>Create</h2></Link>
-    <input placeholder='Search ideas'></input>
+    <Link to = '/Create' className='link'><h2>Create</h2></Link> 
+    <input placeholder='Search ideas' type= " text" value={search} onChange={searchonChange}></input>
+    <button className='searchbutton' onClick={()=>onSearch(search)}>Search</button>
     <Link to = '/Notification' className='link'><p className='icon'><PiNotificationBold size={27}/></p></Link>
     <Link to = '/Chats' className='link'><p className='icon'><BiMessageDetail size={27}/></p></Link>
     <Link to = '/Profile' className='link'><p className='icon'><MdOutlineSwitchAccount size={27}/></p></Link>
